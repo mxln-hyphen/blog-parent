18d2d09e62a2183ccd098c45e99bdab119b00107
Mxln <13924291049@163.com>
2023-08-08 21:38:54 +0800

    2023/8/8 21:38 完成接口:
    其他工作:
    设置缓存相关内容
    
    Bug修复:

--- blog-api/src/main/java/com/itmxln/blog/controller/ArticleController.java
+++ blog-api/src/main/java/com/itmxln/blog/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package com.itmxln.blog.controller;
 
 import com.itmxln.blog.common.aop.LogAnnotation;
+import com.itmxln.blog.common.cache.Cache;
 import com.itmxln.blog.service.ArticleService;
 import com.itmxln.blog.vo.Result;
 import com.itmxln.blog.vo.params.ArticleParam;
@@ -18,52 +19,58 @@ public class ArticleController {
 
     /**
      * 首页 文章列表
+     *
      * @param pageParams
      * @return
      */
     @PostMapping
     //加上此注解，表示要对此接口记录日志
-    @LogAnnotation(module = "文章",operation = "获取文章列表")
-    public Result listArticle(@RequestBody PageParams pageParams){
+    @LogAnnotation(module = "文章", operation = "获取文章列表")
+    public Result listArticle(@RequestBody PageParams pageParams) {
         return articleService.listArticle(pageParams);
     }
 
     /**
      * 首页 最热文章
+     *
      * @return
      */
     @PostMapping("hot")
-    public Result hotArticle(){
-        int limit=5;
+    @Cache(expire = 5 * 60 * 1000, name = "hot_article")
+    public Result hotArticle() {
+        int limit = 5;
         return articleService.hotArticle(limit);
     }
 
     /**
      * 首页 最新文章
+     *
      * @return
      */
     @PostMapping("new")
-    public Result newArticle(){
-        int limit=5;
+    @Cache(expire = 5 * 60 * 1000, name = "new_article")
+    public Result newArticle() {
+        int limit = 5;
         return articleService.newArticle(limit);
     }
 
     /**
      * 首页 文章归档
+     *
      * @return
      */
     @PostMapping("listArchives")
-    public Result listArchives(){
+    public Result listArchives() {
         return articleService.listArchives();
     }
 
     @PostMapping("view/{id}")
-    public Result findArticleById(@PathVariable("id")Long articleId){
+    public Result findArticleById(@PathVariable("id") Long articleId) {
         return articleService.findArticleById(articleId);
     }
 
     @PostMapping("publish")
-    public Result publish(@RequestBody ArticleParam articleParam){
+    public Result publish(@RequestBody ArticleParam articleParam) {
         return articleService.publish(articleParam);
     }
 
